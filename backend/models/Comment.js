6fe3bf33eaba9b12f6a2f5099b10f9e4c4ce254c
Ankitshukla90 <premusha12345@gmail.com>
2026-02-08 13:51:11 +0530

    setting up schemas

---
+++ backend/models/Comment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  videoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Video', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true },
+  userName: String,
+  userAvatar: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('Comment', CommentSchema);
---
+++ backend/models/Video.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const VideoSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  thumbnailUrl: { type: String, required: true },
+  videoUrl: { type: String, required: true },
+  views: { type: Number, default: 0 },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  category: { type: String, default: 'All' },
+  channelName: { type: String, required: true },
+  channelAvatar: { type: String }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Video', VideoSchema);
